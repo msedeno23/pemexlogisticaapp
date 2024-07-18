b6c9bbb90b1b653ec96292c30a57920a393fb5df
msedeno23 <msedeno.blikon@gmail.com>
2024-07-18 14:30:59 -0600

    Swap loginUser and registerUser logic
    The logic between loginUser and registerUser in AuthService.js was mistakenly swapped. This commit corrects the methods to their intended actions. It also adds the functionality to store the registered user's email and any additional data to firestore under the user's unique id (uid).

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,16 +1,23 @@
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
-export const loginUser = async (email, password) => {
+export const registerUser = async (email, password, additionalData) => {
   try {
-    await auth().signInWithEmailAndPassword(email, password);
+    const userCredential = await auth().createUserWithEmailAndPassword(email, password);
+    const { uid } = userCredential.user;
+
+    await firestore().collection('users').doc(uid).set({
+      email,
+      ...additionalData,
+    });
   } catch (error) {
     throw new Error(error.message);
   }
 };
 
-export const registerUser = async (email, password) => {
+export const loginUser = async (email, password) => {
   try {
-    await auth().createUserWithEmailAndPassword(email, password);
+    await auth().signInWithEmailAndPassword(email, password);
   } catch (error) {
     throw new Error(error.message);
   }
