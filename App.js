f3315da9db711a55d106be8052b9ca020ddbf235
msedeno23 <msedeno.blikon@gmail.com>
2024-07-18 17:31:23 -0600

    Add TripSummaryScreen to navigation stack
    The TripSummaryScreen has been imported to the App.js file and added into the application's navigation stack. This new screen will provide a summarized view of trip details for users.

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
+import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import TripDetailsScreen from './src/screens/TripDetailsScreen';
+import TripSummaryScreen from './src/screens/TripSummaryScreen';
 
 const Stack = createStackNavigator();
 
@@ -16,6 +17,7 @@ const App = () => {
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="TripDetails" component={TripDetailsScreen} />
+        <Stack.Screen name="TripSummary" component={TripSummaryScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
